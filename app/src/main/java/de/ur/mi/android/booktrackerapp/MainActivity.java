791f1393487b329d755b9f50332611eb7dd3083e
Nguyet Ha Phung <Nguyet-Ha1.Phung@stud.uni-regensburg.de>
2022-09-10 23:48:26 +0200

    initial commit

---
+++ app/src/main/java/de/ur/mi/android/booktrackerapp/MainActivity.java
@@ -0,0 +1,14 @@
+package de.ur.mi.android.booktrackerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
