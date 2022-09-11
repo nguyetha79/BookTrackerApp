564e37f685b45eadb222ecc775ef4ca75f98b2e3
Nguyet Ha Phung <Nguyet-Ha1.Phung@stud.uni-regensburg.de>
2022-09-11 14:14:45 +0200

    update main

---
+++ app/src/main/java/de/ur/mi/android/booktrackerapp/ShowAllBooks.java
@@ -0,0 +1,14 @@
+package de.ur.mi.android.booktrackerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowAllBooks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_all_books);
+    }
+}
