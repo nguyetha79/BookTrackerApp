da76e9efec9f64d96bea39b9840f78e7e74381dc
Nguyet Ha Phung <Nguyet-Ha1.Phung@stud.uni-regensburg.de>
2022-09-12 13:10:37 +0200

    update

--- app/src/main/java/de/ur/mi/android/booktrackerapp/MainActivity.java
+++ app/src/main/java/de/ur/mi/android/booktrackerapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button continueBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
