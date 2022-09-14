80c16d3e096b4a5a195f5aef2faede26080d211e
Nguyet Ha Phung <Nguyet-Ha1.Phung@stud.uni-regensburg.de>
2022-09-14 15:10:10 +0200

    update

---
+++ app/src/main/java/de/ur/mi/android/booktrackerapp/UpdateBookActivity.java
@@ -0,0 +1,14 @@
+package de.ur.mi.android.booktrackerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_book);
+    }
+}
